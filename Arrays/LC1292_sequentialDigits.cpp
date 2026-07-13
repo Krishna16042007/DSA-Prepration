f47382ab6fc69d95ce6403475986b562c4c027b7
Krishna_Singh <2k24.csaiml1b.2410796@gmail.com>
2026-07-13 21:25:36 +0530

    Add sequentialDigits function in LC1292_sequentialDigits
    Implement a solution to find all sequential digits within a given range.

---
+++ Arrays/LC1292_sequentialDigits.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        vector<int> ans;
+
+        queue<int> q;
+        for (int i = 1; i <= 9; i++) {
+            q.push(i);
+        }
+
+        while (!q.empty()) {
+            int num = q.front();
+            q.pop();
+
+            if (num >= low && num <= high)
+                ans.push_back(num);
+
+            int lastDigit = num % 10;
+
+            if (lastDigit < 9) {
+                q.push(num * 10 + (lastDigit + 1));
+            }
+        }
+
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
